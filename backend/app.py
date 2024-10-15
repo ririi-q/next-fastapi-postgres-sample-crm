994430456588d79739f8b191aa39076d775bc32b
ririi-q <ririi.q.27@gmail.com>
2024-10-15 17:09:37 +0000

    backend setup

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+@app.route("/api/v1/healthcheck")
+def healthcheck():
+    return {"message": "OK"}
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
